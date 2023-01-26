bc028afff4c11fa26d03706929ebafe6e9dd175b
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2023-01-25 21:52:39 -0800

    Fixed generating new variable len chromosomes

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ def parse_args():
     parser.add_argument('-k', '--tours', dest='k_values', type=str, required=True, help='k number of tours. ex: -k 2,4,8')
     parser.add_argument('-md', '--multi_depot', dest='multi_depot_routing', type=bool, default=False, required=False, help='build tours using multiple depots')
     parser.add_argument('-s', '--seeds', dest='seeds', type=str, required=True, help='random seeds to run the ga. ex: -s 1234,3949')
+    parser.add_argument('-j', '--heuristics', dest='heuristics', type=str, default='MAMR', required=False, help='the set of heuristics (MAMR, RR). ex: -j MAMR')
+
     args = parser.parse_args()
     # check and adjust the parsed args
     args.instance = args.instance.replace(' ', '')
@@ -25,6 +27,7 @@ def parse_args():
         sys.exit("Cannot find instance: " + args.instance)
     args.k_values = [int(i) for i in args.k_values.split(',')]
     args.seeds = [int(i) for i in args.seeds.split(',')]
+    args.heuristics = args.heuristics.replace(' ', '')
     return args
 
 def main():
@@ -43,11 +46,11 @@ def main():
         # create the evaluator
         evalor = evaluator.Evaluator(gph, k)
         evalor.depotNode = 0
-        if True: # TODO 
-            evalor.geneLength = 2 # the number of heuristics
+        if args.heuristics == 'MAMR': # TODO 
+            evalor.geneLength = 2 # 4 total heuristics
             evalor.chromeLength = gph.size_e() * evalor.geneLength
-        else:
-            evalor.geneLength = 2 # the number of heuristics
+        elif args.heuristics == 'RR':
+            evalor.geneLength = len(bin(gph.maxVertexDegree)[2:])
             evalor.chromeLength = gph.size_e() * evalor.geneLength
 
         # create the genetic algorithm with the evaluator
